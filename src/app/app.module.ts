709dc905bf64113b681a6e558df85842ca24825e
Brenden <52762708+brendenmoore@users.noreply.github.com>
2020-05-25 10:24:09 -0400

    refactor details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, RecipeListComponent],
+  declarations: [AppComponent, RecipeListComponent, RecipeDetailComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
