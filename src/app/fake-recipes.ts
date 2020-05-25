2241d0eb4ac9810d7a8f8c29b2b63e1c0f909f47
Brenden <52762708+brendenmoore@users.noreply.github.com>
2020-05-25 10:13:19 -0400

    fake recipes

---
+++ src/app/fake-recipes.ts
@@ -0,0 +1,10 @@
+import { Recipe } from "./recipe";
+
+export const RECIPES: Recipe[] = [
+  { id: 1, name: "Pizza" },
+  { id: 2, name: "Salad" },
+  { id: 3, name: "Spaghetti" },
+  { id: 4, name: "Lasagna" },
+  { id: 5, name: "Cereal" },
+  { id: 6, name: "Toast" },
+];
