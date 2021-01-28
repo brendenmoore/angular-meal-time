d097cf38ab46b8c6bf8d3b2965bedb8f75b21e21
T-Mev <teddirect@gmail.com>
2021-01-27 23:24:07 -0500

    Added background color to navbar on scroll and positioning fixes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject } from '@angular/core';
+import { $ } from 'protractor';
 import { UserService } from '../user.service';
 
 @Component({
@@ -6,8 +8,18 @@ import { UserService } from '../user.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent{
+export class HeaderComponent {
 
-  constructor(public user: UserService) {}
+  constructor(public user: UserService, @Inject(DOCUMENT) private document: Document) { }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (document.body.scrollTop > 150 ||
+      document.documentElement.scrollTop > 150) {
+      document.getElementById('navbarMain').classList.add('scrolled');
+    } else {
+      document.getElementById('navbarMain').classList.remove('scrolled');
+    }
+  }
 
 }
